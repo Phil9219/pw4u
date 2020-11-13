1cccd507a867c912ff205b9af67eb06bb5ea843b
Phil9219 <philipp.fell@yahoo.de>
2020-11-13 11:56:11 +0100

    Add .masterPassword + Bugfixes

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 const { readCommandLineArguments } = require("./lib/commandLine");
+const { close, connect } = require("./lib/database");
 const { getPassword, setPassword } = require("./lib/passwords");
 const { askForMasterPassword } = require("./lib/questions");
 const { isMasterPasswordCorrect } = require("./lib/validation");
 
 async function run() {
+  await connect(
+    "mongodb+srv://phil:rceTBvhDn9suKXY8@cluster0.umnb6.mongodb.net/PasswordSaver?retryWrites=true&w=majority",
+    "PasswordSaver"
+  );
+  console.log("connect to db");
+
   const masterPassword = await askForMasterPassword();
 
   if (!(await isMasterPasswordCorrect(masterPassword))) {
@@ -24,6 +31,7 @@ async function run() {
     const passwordValue = await getPassword(passwordName);
     console.log(`Your password is ${passwordValue} 🎉`);
   }
+  await close();
 }
 
 run();
